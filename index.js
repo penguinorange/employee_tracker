82579e62b75791bebbc8cdd3954e18846dd8ca12
jellyfish <johnrclifford@gmail.com>
2020-10-27 18:54:14 -0500

    template and directory created

---
+++ index.js
@@ -0,0 +1,17 @@
+const util = require("util");
+const mysql = require("mysql");
+
+const connection = mysql.createConnection({
+  host: "localhost",
+  // Your username
+  user: "root",
+  // Your password
+  password: "rootroot",
+  database: "employees"
+});
+
+connection.connect();
+
+// Setting up connection.query to use promises instead of callbacks
+// This allows us to use the async/await syntax
+connection.query = util.promisify(connection.query);
