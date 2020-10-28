2c1813563f5047dd0e6aff95f2b7028b9e202cf1
jellyfish <johnrclifford@gmail.com>
2020-10-27 19:22:42 -0500

    switch-cases on opening prompts added

--- index.js
+++ index.js
@@ -1,13 +1,14 @@
 const util = require("util");
 const mysql = require("mysql");
+const inquirer = require("inquirer");
 
 const connection = mysql.createConnection({
-  host: "localhost",
-  // Your username
-  user: "root",
-  // Your password
-  password: "rootroot",
-  database: "employees"
+    host: "localhost",
+    // Your username
+    user: "root",
+    // Your password
+    password: "rootroot",
+    database: "employees"
 });
 
 connection.connect();
@@ -15,3 +16,81 @@ connection.connect();
 // Setting up connection.query to use promises instead of callbacks
 // This allows us to use the async/await syntax
 connection.query = util.promisify(connection.query);
+
+function mainPrompts() {
+    inquirer.prompt([{
+        type: "list",
+        name: "choice",
+        message: "What would you like to do?",
+        choices: [
+            {
+                name: "View Departments",
+                value: "VIEW_DEPARTMENTS"
+            },
+            {
+                name: "Add Departments",
+                value: "ADD_DEPARTMENTS"
+            },
+            {
+                name: "View Roles",
+                value: "VIEW_ROLES"
+            },
+            {
+                name: "Add Roles",
+                value: "ADD_ROLES"
+            },
+            {
+                name: "View Employees",
+                value: "VIEW_EMPLOYEES"
+            },
+            {
+                name: "Add Employees",
+                value: "ADD_EMPLOYEES"
+            },
+            {
+                name: "Update Employee Role",
+                value: "UPDATE_EMPLOY_ROLE"
+            },
+            {
+                name: "Exit",
+                value: "EXIT"
+            }
+        ]
+    }]).then(({ choice }) => {
+        switch (choice) {
+            case "VIEW_DEPARTMENTS":
+                displayDepartments();
+                break;
+            case "ADD_DEPARTMENTS":
+                addDepartments();
+                break;
+            case "VIEW_ROLES":
+                displayRoles();
+                break;
+            case "ADD_ROLES":
+                addRoless();
+                break;
+            case "VIEW_EMPLOYEES":
+                displayEmployees();
+                break;
+            case "ADD_EMPLOYEES":
+                addEmployees();
+                break;
+            case "UPDATE_EMPLOY_ROLE":
+                updateEmployRole();
+                break;
+            case "EXIT":
+            default:
+                process.exit();
+        }
+    })
+}
+
+function displayDepartments() {
+    console.log("\n");
+    connection.query("SELECT * FROM department").then(response => {
+        console.table(response);
+        mainPrompts();
+    })
+}
+
